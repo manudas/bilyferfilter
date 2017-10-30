2d333eb87a1abb739c30c4c5d9e7f5a108be3023
Manuel José Pulgar Anguita <manuel.pulgar@affilired.com>
2017-10-30 15:38:29 +0100

    Filtering seems to start working. Integrated with blocklayered module

--- bilyferfilterbyattribute.php
+++ bilyferfilterbyattribute.php
@@ -50,7 +50,8 @@ class Bilyferfilterbyattribute extends Module
 	public function install()
 	{
 		if (!parent::install() || !$this->registerHook('actionProductListOverride')
-				|| !$this->registerHook('aboveProductList'))
+				|| !$this->registerHook('aboveProductList')
+                || !$this->registerHook('actionProductListModifier'))
 			return false;
 		return true;
 	}
@@ -65,24 +66,85 @@ class Bilyferfilterbyattribute extends Module
             $attribute_group['attributes'] = $attributes;
 	    }
 
-		$this -> smarty -> assign(array('attribute_groups' => $attribute_groups));
+	    $ordenation = Tools::getValue('hidden_selectProductSort');
+	    $n = Tools::getValue('hidden_n');
+	    $p = Tools::getValue('hidden_p');
+		$this -> smarty -> assign(array('attribute_groups' => $attribute_groups,
+                                        'ordenation' => $ordenation,
+                                        'form_n' => $n,
+                                        'form_p' => $p)
+        );
 				//return $this->display( _PS_MODULE_DIR_.'bilyferfilterbyattribute/views/templates/hook/product-sort.tpl');
 // return $this->display('/var/www/html/ps16/modules/bilyferfilterbyattribute/views/templates/hook/product-sort.tpl');
 		return $this->display(__FILE__, 'product-sort-bilyfer.tpl');
 	}
 
-	public function hookActionProductListOverride ($params) {
-		/* ESTRUCTURA DEL HOOK
-		Hook::exec('actionProductListOverride', array(
+
+	public function hookActionProductListModifier($params){
+	    /* PRODUCTS ARE ALREADY PASSED IN PARAMS
+        Hook::exec(
+            'actionProductListModifier',
+            array(
+                'nb_products' => &$nb_products,
+                'cat_products' => &$products,
+            )
+        );
+	    */
+        //hacer
+        /*
+        @todo
+        */
+
+        $filtered_products = $this->filterByAttribute($params['cat_products']);
+        $nb_products = count($filtered_products);
+
+        $params['cat_products'] = $filtered_products;
+        $params['nb_products'] = $nb_products;
+    }
+
+
+    private function filterByAttribute($product_list)
+    {
+
+        $availabe_att_groups = Tools::getValue('filterByAttributeGroup');
+        $result_cat_products = array();
+
+        $attribute_list = array();
+        foreach ($availabe_att_groups as $id_att_group => $attribute_value_to_filter_by) {
+            if (empty($attribute_value_to_filter_by)) {
+                continue;
+            }
+            else {
+                $attribute_list[] = $attribute_value_to_filter_by;
+            }
+        }
+
+        if (!empty($attribute_list)) {
+            foreach ($product_list as $product) {
+                $product_obj = new Product($product['id_product']);
+                if ($product_obj->productAttributeExists($attribute_list)) {
+                    $result_cat_products[] = $product;
+                }
+            }
+        }
+
+        return $result_cat_products;
+    }
+
+
+	public function hookActionProductListOverride ($params)
+    {
+        /* ESTRUCTURA DEL HOOK
+        Hook::exec('actionProductListOverride', array(
             'nbProducts'   => &$this->nbProducts,
             'catProducts'  => &$this->cat_products,
             'hookExecuted' => &$hook_executed,
         ));
-		*/
+        */
 
-		/* FORMA DE FUNCIONAR DEL FILTRO DE CATEGORIA
+        /* FORMA DE FUNCIONAR DEL FILTRO DE CATEGORIA
 
-		        // The hook was not executed, standard working
+                // The hook was not executed, standard working
         if (!$hook_executed) {
             $this->context->smarty->assign('categoryNameComplement', '');
             $this->nbProducts = $this->category->getProducts(null, null, null, $this->orderBy, $this->orderWay, true);
@@ -90,17 +152,55 @@ class Bilyferfilterbyattribute extends Module
             $this->cat_products = $this->category->getProducts($this->context->language->id, (int)$this->p, (int)$this->n, $this->orderBy, $this->orderWay);
         }
 
-		*/
-/*
-		$id_category = Tools::getValue('id_category');
-		$category = new Category($id_category);
+        */
 
-		$params['nbProducts'] = $category->getProducts(null, null, null, $this->orderBy, $this->orderWay, true);
+        $order = Tools::getValue('hidden_selectProductSort');
+        $order_arr = explode(':', $order);
+        $order_by = $order_arr [0];
+        $order_way = $order_arr[1];
 
-        $params['cat_products'] = $category->getProducts($this->context->language->id, (int)$this->p, (int)$this->n, $this->orderBy, $this->orderWay);
+        $id_category = Tools::getValue('id_category');
+        $category = new Category($id_category);
+
+        $nbProducts = $category->getProducts(null, null, null, $order_by, $order_way, true);
+
+
+        $n = Tools::getValue('n');
+        $p = Tools::getValue('p');
+
+        if ($p == false) {
+            $p = 0;
+        }
+        if ($n == false) {
+            $n = Configuration::get('PS_PRODUCTS_PER_PAGE');
+        }
+        $cat_products = $category->getProducts($this->context->language->id, $p, $n, $order_by, $order_way);
+
+
+        $result_cat_products = $this->filterByAttribute($cat_products);
+
+
+        if (!empty($result_cat_products)){
+            // productAttributeExists($attributes_list
+            $result_nb_cat_products = count($result_cat_products);
+
+            $params['nbProducts'] = $result_nb_cat_products;
+            $params['catProducts'] = $result_cat_products;
+            $params['hookExecuted'] = true;
+        }
+        else {
+            $params['hookExecuted'] = false;
+        }
+		/*
+		$params['nbProducts'] = $category->getProducts(null, null, null, $order_by, $order_way, true);
+
+        // $params['cat_products'] = $category->getProducts($this->context->language->id, (int)$this->p, (int)$this->n, $order_by, $order_way);
+        // we don't have $this -> p and $this -> n so... or we include it in other hidden inputs or... we set it to null
+        // due to te fact that I believe they are included in cookies, I'll set it to null in order to test
+        $params['cat_products'] = $category->getProducts($this->context->language->id, null, null, $order_by, $order_way);
         $filter_by_material = Tool::getValue();
-
 */
+
 	}
 
 	public function uninstall()
--- views/templates/hook/product-sort-bilyfer.tpl
+++ views/templates/hook/product-sort-bilyfer.tpl
@@ -1,11 +1,58 @@
+{if !isset($request)}
+	<!-- Sort products -->
+    {if isset($smarty.get.id_category) && $smarty.get.id_category}
+        {assign var='request' value=$link->getPaginationLink('category', $category, false, true)
+        }	{elseif isset($smarty.get.id_manufacturer) && $smarty.get.id_manufacturer}
+        {assign var='request' value=$link->getPaginationLink('manufacturer', $manufacturer, false, true)}
+    {elseif isset($smarty.get.id_supplier) && $smarty.get.id_supplier}
+        {assign var='request' value=$link->getPaginationLink('supplier', $supplier, false, true)}
+    {else}
+        {assign var='request' value=$link->getPaginationLink(false, false, false, true)}
+    {/if}
+{/if}
+
+<form method="post" id="productsSortFormByAttribute{if isset($paginationId)}_{$paginationId}{/if}" action="{$request|escape:'html':'UTF-8'}" class="productsSortFormByAttribute">
+
+	<input name='hidden_selectProductSort' id='hidden_selectProductSort' type="hidden" value="{$ordernation}" />
+	<script>
+
+        $(window).load(function(){
+			var product_sort_selector = $('#selectProductSort{if isset($paginationId)}_{$paginationId}{/if}');
+            product_sort_selector.on('change', function(){
+				$('#hidden_selectProductSort').val($('#selectProductSort{if isset($paginationId)}_{$paginationId}{/if}').val());
+			});
+            $('#hidden_selectProductSort').val($('#selectProductSort{if isset($paginationId)}_{$paginationId}{/if}').val());
+            // alert('n and p');
 
 
-<form id="product...sSortForm{if isset($paginationId)}_{$paginationId}{/if}" action="{$request|escape:'html':'UTF-8'}" class="pro...ductsSortForm">
+			var serialized_data = $('#productsSortFormByAttribute{if isset($paginationId)}_{$paginationId}{/if}').serialize();
+
+
+
+
+            $(document).off('change', '.selectProductSort{if isset($paginationId)}_{$paginationId}{/if}').on('change', '.selectProductSort', function(e) {
+                $('.selectProductSort{if isset($paginationId)}_{$paginationId}{/if}').val($(this).val());
+
+                if($('#layered_form').length > 0)
+                    reloadContent('&forceSlide&'+serialized_data);
+            });
+
+            $(document).off('change', 'select[name="n"]').on('change', 'select[name="n"]', function(e)
+            {
+                $('select[name=n]').val($(this).val());
+                reloadContent('&forceSlide&'+serialized_data);
+            });
+
+
+			// $sort_form = $("productsSortForm{if isset($paginationId)}_{$paginationId}{/if}");
+		});
+
+	</script>
     {*$attribute_groups|var_dump*}
 	{foreach $attribute_groups as $grupo_atributo}
         {*$grupo_atributo|var_dump*}
-		<select id="filterByAttributeGroup_{$grupo_atributo.id_attribute_group}" class="selectProdu...ctSort form-cont...rol" name="filterByAttributeGroup_{$grupo_atributo['id_attribute_group']}">
-
+		<select id="filterByAttributeGroup_{$grupo_atributo.id_attribute_group}" class="selectProdu...ctSort form-cont...rol" name="filterByAttributeGroup[{$grupo_atributo['id_attribute_group']}]">
+			<option value="">---</option>
 			{foreach $grupo_atributo.attributes as $single_atributo}
                 {*$single_atributo|var_dump*}
 				<option value="{$single_atributo.id_attribute}" {if $orderby eq 'reference' AND $orderway eq 'asc'}selected="selected"{/if}>{$single_atributo.name}</option>
@@ -15,6 +62,7 @@
 		</select>
 	
 	{/foreach}
+	<button type="submit">{l s='Filter' mod='bilyferfilterbyattribute'}</button>
 
 </form>
 
